d6e64d92b81ee2f8c1d7ac6407fbc47b19373e70
GUSTAVO FERRAZ SANTOS <110568302+gustavoferrazdesousa@users.noreply.github.com>
2023-09-28 22:29:48 -0300

    Preparacao para a proxima aula

---
+++ src/test/java/modulos/produtos/ProdutoTest.java
@@ -0,0 +1,14 @@
+package modulos.produtos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testes de API Rest do modulo de Produtos")
+public class ProdutoTest {
+    @Test
+    @DisplayName("Validar os limites proibidos do valor do produto")
+    public void testValidarLimitesProibidosValorProduto(){
+        //
+    }
+}
